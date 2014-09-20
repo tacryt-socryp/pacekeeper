5350141254cee289f713d6d5e9753cf56c1d3944
Logan Allen <loganallc@gmail.com>
2014-09-20 06:36:54 -0400

    Cards implementation for Wear

--- wear/src/main/java/htn/logan/runningdj/WearActivity.java
+++ wear/src/main/java/htn/logan/runningdj/WearActivity.java
@@ -2,23 +2,36 @@ package htn.logan.runningdj;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
+import android.support.wearable.view.GridViewPager;
+import android.content.res.Resources;
+import android.view.View;
+import android.view.View.OnApplyWindowInsetsListener;
+import android.view.WindowInsets;
+import com.google.android.gms.common.*;
+import com.google.android.gms.common.api.*;
+//import com.google.android.gms.fitness.*;
 
 public class WearActivity extends Activity {
-
-    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wear);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+        final Resources res = getResources();
+        final GridViewPager pager = (GridViewPager) findViewById(R.id.pager);
+        pager.setOnApplyWindowInsetsListener(new OnApplyWindowInsetsListener() {
             @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                // Adjust page margins:
+                //   A little extra horizontal spacing between pages looks a bit
+                //   less crowded on a round display.
+                final boolean round = insets.isRound();
+                int rowMargin = res.getDimensionPixelOffset(R.dimen.page_row_margin);
+                int colMargin = res.getDimensionPixelOffset(R.dimen.page_row_margin);
+                pager.setPageMargins(rowMargin, colMargin);
+                return insets;
             }
         });
+        pager.setAdapter(new GridPagerAdapter(this, getFragmentManager()));
     }
 }
